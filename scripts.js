ace63144e034aa215b28b684d2298e09e68d5117
Benjamin <kiptoo925@gmail.com>
2023-11-16 20:38:37 +0300

    Weather icons changing according to current weather conditions

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon")
 
 
 async function checkWeather(city) {
@@ -15,6 +16,19 @@ async function checkWeather(city) {
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "images/cloudy.png"
+    } else if (data.weather[0].main == "Clear"){
+        weatherIcon.src = "images/sun.png"
+    } else if (data.weather[0].main == "Rain"){
+        weatherIcon.src = "images/rainy.png"
+    } else if (data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "images/drizzle.png"
+    } else if (data.weather[0].main == "Mist"){
+        weatherIcon.src = "images/foggy-night.png"
+    } 
 
 
 }
